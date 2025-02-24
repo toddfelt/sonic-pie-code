4cf5f72e62511d228b9497216458172ad7524651
toddfelt <todd_felt@student.davincischools.org>
2025-02-24 10:33:44 -0800

    Create ChallengeE.rb

---
+++ ChallengeE.rb
@@ -0,0 +1,41 @@
+# Challenge E
+use_bpm 180
+use_synth :chipbass
+
+# VARIABLES TO STORE SAMPLES
+
+
+# IT'S A ME, MARIO! SAMPLE
+sample "C:/Users/todd_felt/Downloads/mario_sounds/mario_intro.wav"
+sleep 8
+
+# INTRO
+play :e4
+sleep 0.5
+play :e4
+sleep 1
+play :e4
+sleep 1
+play :c4
+sleep 0.5
+play :e4
+sleep 1
+play :g4
+sleep 2
+play :g3
+sleep 2
+
+
+# CUSTOM MARIO SAMPLES
+sample "C:/Users/todd_felt/Downloads/mario_sounds/coin.wav"
+sleep 2
+sample "C:/Users/todd_felt/Downloads/mario_sounds/oneup.wav"
+sleep 2
+sample "C:/Users/todd_felt/Downloads/mario_sounds/pipe.wav"
+sleep 2
+sample "C:/Users/todd_felt/Downloads/mario_sounds/jump.wav"
+sleep 2
+
+
+# MUSHROOM SAMPLE
+"C:/Users/todd_felt/Desktop/Super Mario Bros. - Mushroom Sound Effect"
