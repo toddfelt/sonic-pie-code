b69f9779cb09161d4cf9725c31b75f4b44aa73c5
toddfelt <todd_felt@student.davincischools.org>
2025-03-10 10:46:20 -0700

    Update challengeG.rb

--- challengeG.rb
+++ challengeG.rb
@@ -16,3 +16,23 @@ live_loop :main_theme do
     i = 0
   end
 end
+
+# Seven Nation Army by The White Stripes challenge G
+
+use_bpm 120
+use_synth :fm
+use_synth_defaults sustain: 0.25
+
+
+notes = [:e3,:e3,:g3,:e3,:r,:d3,:c3,:b2 ]
+i = 0
+
+live_loop :white_stripes do
+  8.times do
+    play(notes[i])
+    sleep 0.5
+    i = i + 1
+    print(i)
+  end
+  i = 0
+end
