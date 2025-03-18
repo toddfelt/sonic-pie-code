416dfbe83c5c131a8bd455e16aff62d36c1e41d5
toddfelt <todd_felt@student.davincischools.org>
2025-03-18 15:00:01 -0700

    Update runawayRemix.rb

--- runawayRemix.rb
+++ runawayRemix.rb
@@ -2,8 +2,7 @@
 index = 0
 use_bpm 85
 use_synth :piano
-carti = -"C:/Users/todd_felt/Desktop/Playboi Carti - WHAT Sound EffectAdlib.mp4"
-
+carti = - "C:/Users/todd_felt/Desktop/Playboi Carti - WHAT Sound EffectAdlib.mp3"
 tecca = "C:/Users/todd_felt/Downloads/We love you Tecca Prod Tag.mp3"
 define :kanye do
   play :gs3
@@ -78,7 +77,7 @@ with_fx :reverb, mix: 0.5 do
   10.times do
     # Fade-in
     play :c4, amp: line(0, 0.5, steps: 10).tick, release: 2
-    
+    sample carti
     # Fade-out with gradual decrease in amplitude
     control amp: line(0.5, 0, steps: 10).tick
     sleep 0.5
